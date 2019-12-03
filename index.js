cf246537ab3e21ab8ff2056109193eb30ea9d0fc
c5apple <c5apple_party@yahoo.co.jp>
2019-12-03 20:20:33 +0900

    圧縮率変更

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 
 // 画像圧縮
 const MAX_WIDTH = (process.argv.length > 2) ? process.argv[2] : 975;
-const QUALITY = 65;
+const QUALITY = 50;
 
 fs.readdir('dist', (err, files) => {
   if (err) throw err;
